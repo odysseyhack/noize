71d44307e7dd60be5467ca9c81236f30acb59c9d
Giovanni <giovanni.balestrieri@inria.fr>
2019-04-14 01:00:38 +0200

    Working on LOGIN and LOGOUT AAAAAAAAHHHH

--- datab/datab/settings.py
+++ datab/datab/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap3',
     'accounts',
 ]
 
@@ -121,3 +122,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIR = [os.path.join(BASE_DIR,'static')]
+
+LOGIN_REDIRECT_URL = "test"
+LOGOUT_REDIRECT_URL = "thanks"
